dbde7bc78695cc3ae0a3b32092ef83b0e7d02c18
savva <zulvut.sk@gmail.com>
2023-03-08 19:20:58 +0300

    0.0.9 -renamed file func add -cancel create note add -recolor float.act.btn

--- app/src/main/java/ru/zulvit/vknote/MainActivity.java
+++ app/src/main/java/ru/zulvit/vknote/MainActivity.java
@@ -10,6 +10,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Chronometer;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
@@ -30,15 +31,15 @@ public class MainActivity extends AppCompatActivity {
     private MediaRecorder recorder;
     private Chronometer chronometer;
     private boolean recordState;
-
     private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
     private boolean permissionToRecordAccepted = false;
     private final String[] permissions = {Manifest.permission.RECORD_AUDIO,
             Manifest.permission.WRITE_EXTERNAL_STORAGE,
             Manifest.permission.READ_EXTERNAL_STORAGE};
-
     private static final int SAMPLE_RATE = 44100;
     private static final int BIT_RATE = 96000;
+
+    private String dirPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +50,9 @@ public class MainActivity extends AppCompatActivity {
 
         FloatingActionButton floatingActionButton = findViewById(R.id.button_record);
         chronometer = findViewById(R.id.chronometer);
+
+        Context context = getApplicationContext();
+        dirPath = context.getFilesDir().getAbsolutePath() + "/notes/";
 
         recordState = false;
 
@@ -83,23 +87,56 @@ public class MainActivity extends AppCompatActivity {
                 .inflate(R.layout.bottom_sheet,
                         (LinearLayout) findViewById(R.id.bottomSheetContainer));
         bottomSheetDialog.setContentView(bottomSheetView);
+        EditText editText = bottomSheetDialog.findViewById(R.id.editTextNoteName);
+        assert editText != null;
+        editText.setText(getDataAndTime());
         bottomSheetDialog.show();
 
         bottomSheetView.findViewById(R.id.buttonSheetSave).setOnClickListener(view -> {
-            Log.d("SAVE", "SAVING");
+            File lastCreatedFile = getLastCreatedFile();
+            if (lastCreatedFile != null) {
+                File renamedLastFile = new File(dirPath + editText.getText());
+                boolean success = lastCreatedFile.renameTo(renamedLastFile);
+                if (success) {
+                    Log.d("Saving...", "success");
+                } else {
+                    Log.d("Saving...", "file err");
+                }
+            }
+            bottomSheetDialog.cancel();
             replaceFragment(new NotesFragment());
         });
 
         bottomSheetView.findViewById(R.id.buttonSheetCancel).setOnClickListener(view -> {
+            File lastCreatedFile = getLastCreatedFile();
+            boolean success = lastCreatedFile.delete();
+            if (success) {
+                Log.d("Canceling...", "success");
+            } else {
+                Log.d("Canceling...", "err");
+            }
             bottomSheetDialog.cancel();
-            Log.d("CANCEL", "CANCELING");
             replaceFragment(new NotesFragment());
         });
     }
 
-    private void recordStart() {
-        Context context = getApplicationContext();
-        String dirPath = context.getFilesDir().getAbsolutePath() + "/notes/";
+    private File getLastCreatedFile() {
+        File folder = new File(dirPath);
+        File[] folderFiles = folder.listFiles();
+        File lastCreatedFile = null;
+        long newestTime = Long.MIN_VALUE;
+        if (folderFiles != null) {
+            for (File file : folderFiles) {
+                if (file.isFile() && file.lastModified() > newestTime) {
+                    lastCreatedFile = file;
+                    newestTime = file.lastModified();
+                }
+            }
+        }
+        return lastCreatedFile;
+    }
+
+    private String getDataAndTime() {
         Calendar calendar = Calendar.getInstance();
         int year = calendar.get(Calendar.YEAR);
         int month = calendar.get(Calendar.MONTH);
@@ -107,18 +144,22 @@ public class MainActivity extends AppCompatActivity {
         int hour = calendar.get(Calendar.HOUR_OF_DAY);
         int minute = calendar.get(Calendar.MINUTE);
         int second = calendar.get(Calendar.SECOND);
-        String defaultTitleNote = year + "-" + month + "-" + day + "(" +
-                hour + "-" + minute + "-" + second + ")";
+        return year + "-" + month + "-" + day + "(" +
+                hour + ":" + minute + ":" + second + ")";
+    }
+
+    private void recordStart() {
+        String titleNote = getDataAndTime();
+        File dir = new File(dirPath);
+        if (dir.mkdirs()) {
+            dirPath += titleNote;
+            Log.d(MainActivity.class.getName(), dirPath);
+            Log.d(MainActivity.class.getName(), "note dir created");
+        }
+        File file = new File(dir, titleNote + ".m4a");
         try {
             chronometer.setBase(SystemClock.elapsedRealtime());
             chronometer.start();
-            File dir = new File(dirPath);
-            if (dir.mkdirs()) {
-                dirPath += defaultTitleNote;
-                Log.d(MainActivity.class.getName(), dirPath);
-                Log.d(MainActivity.class.getName(), "note dir created");
-            }
-            File file = new File(dir, defaultTitleNote + ".m4a");
             recorder = new MediaRecorder();
             recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
             recorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
